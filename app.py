e036b086e6aab7186b87e49b5d9c56582167f1a4
Krutyy Mikhail Ostapovich <krutyy.mikhail@gmail.com>
2021-08-30 06:59:45 +0300

    refactoring, rendering departures

--- app.py
+++ app.py
@@ -1,48 +1,45 @@
 import flask
 from flask import render_template
 
-import data
-
+from db import db
 
 app = flask.Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def index_view():
+    all_tours = db.get_all_tours()
+    return render_template('index.html', tours=all_tours)
 
 
 @app.route('/departures/<departure>')
-def departure(departure):
-    return render_template('departure.html')
+def departure_view(departure):
+    tours_on_departure = db.get_tours_on_departure(departure)
+    min_price_tour = db.get_min_price_tour(tours_on_departure)
+    max_price_tour = db.get_max_price_tour(tours_on_departure)
+    min_night_tour = db.get_min_night_tour(tours_on_departure)
+    max_night_tour = db.get_max_night_tour(tours_on_departure)
+    departure_desc = db.get_departure_desc(departure)
+    return render_template('departure.html',
+                           tours_on_departure=tours_on_departure,
+                           departure=departure,
+                           departure_desc=departure_desc,
+                           min_price_tour=min_price_tour,
+                           max_price_tour=max_price_tour,
+                           min_night_tour=min_night_tour,
+                           max_night_tour=max_night_tour,
+                           )
 
 
-@app.route('/tours/<id>')
-def tour(id):
-    return render_template('tour.html')
-
-
-@app.route('/data/')
-def all_tours():
-    return render_template('tours.html', tours=data.tours)
-
-
-@app.route('/data/departures/<departure>')
-def tours_on_departure(departure):
-    tours_on_departure = {}
-
-    for tour in data.tours:
-        print(data.tours[tour])
-        if data.tours[tour]['departure'] == departure:
-            tours_on_departure[tour] = data.tours[tour]
-
-    return render_template('tours_on_departure.html', tours_on_departure=tours_on_departure, departure=data.departures[departure])
-
-
-@app.route('/data/tours/<int:id>')
-def tour_desc(id):
-    return render_template('tour_desc.html', tour=data.tours[id])
+@app.route('/tours/<int:tour_id>/')
+def tour_view(tour_id):
+    tour = db.get_tour(tour_id)
+    str_departure = db.get_str_departure(tour);
+    return render_template('tour.html',
+                           tour=tour,
+                           str_departure=str_departure
+                           )
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run('127.0.0.1',8000)
---
+++ db/db.py
@@ -0,0 +1,42 @@
+from db import data
+
+
+def get_all_tours():
+    return data.tours
+
+
+def get_tour(tour_id):
+    return data.tours[tour_id]
+
+
+def get_str_departure(tour):
+    return data.departures[tour["departure"]]
+
+
+def get_tours_on_departure(departure):
+    tours_on_departure = {key: value for key, value in data.tours.items() if value['departure'] == departure}
+
+    return tours_on_departure
+
+
+def _get_values_on_field(data, field):
+    return (obj[field] for obj in data.values())
+
+
+def get_min_price_tour(tours_on_departure):
+    return min(_get_values_on_field(tours_on_departure, 'price'))
+
+
+def get_max_price_tour(tours_on_departure):
+    return max(_get_values_on_field(tours_on_departure, 'price'))
+
+def get_min_night_tour(tours_on_departure):
+    return min(_get_values_on_field(tours_on_departure, 'nights'))
+
+
+def get_max_night_tour(tours_on_departure):
+    return max(_get_values_on_field(tours_on_departure, 'nights'))
+
+
+def get_departure_desc(departure):
+    return data.departures[departure]
